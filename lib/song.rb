e043681d60d4655d885b18da6378b0ac92deb7ab
Christopher Black <christopher.black.2209@googlemail.com>
2021-04-16 22:46:23 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,7 +10,7 @@ class Song
     @artist=(artist)
     @genre=(genre)
     @@count += 1 
-    @@genres << genre if (@@genres.include?(genre))
+    @@genres << genre if @@genres.include?(genre)
     @@artists << artist if !(@@artists.include?(artist))
      # @@genres << genre
     #end 
