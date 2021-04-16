74ed95555f7f371a587eebcd4b28875b02868056
Christopher Black <christopher.black.2209@googlemail.com>
2021-04-16 22:00:55 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,11 +1,35 @@
 class Song
   attr_reader :name, :artist, :genre 
   
+  @@count = 0 
+  @@artists = []
+  @@genres = []
+  
   def initialize(name,artist,genre)
     @name=(name)
     @artist=(artist)
     @genre=(genre)
+    @@count += 1 
   end 
   
+  def self.count
+    @@album_count
+  end 
+  
+  def self.artists
+    @@artists
+  end
+  
+  def self.genres
+    @@genres
+  end
+  
+  def self.genre_count
+    
+  end 
+  
+  def self.artist_count
+    
+  end 
   
 end 
